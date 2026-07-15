edf03bfbbd34455cf868d7af5daac97aaf7d7fd4
ItayELY <itay.elyashiv@gmail.com>
2026-07-15 11:33:36 +0000

    all plus petri net fix

--- benchmarks/classes/Envs.py
+++ benchmarks/classes/Envs.py
@@ -4,10 +4,10 @@ import os
 class Envs:
 
     def __init__(self):
-        self.experiment = os.getenv("EXPERIMENT_NAME", "AGAINST_BV_NEW_INSTANCES")
+        self.experiment = os.getenv("EXPERIMENT_NAME", "PETRINET")
         self.index = int(os.getenv("WORKER_INDEX", os.getenv("AWS_BATCH_JOB_ARRAY_INDEX", 0)))
         self.instances = int(os.getenv("INSTANCES_PER_MACHINE", 10))
         self.startFrom = int(os.getenv("INSTANCES_START_FROM", 0))
-        self.timeout = int(os.getenv("TIMEOUT", 120))
-        self.file = os.getenv("FILE", "benchmarks/instances-patty-bv.csv")
+        self.timeout = int(os.getenv("TIMEOUT", 300))
+        self.file = os.getenv("FILE", "benchmarks/instances-petrinet.csv")
         self.isInsideAWS = "AWS_BATCH_JOB_ID" in os.environ
